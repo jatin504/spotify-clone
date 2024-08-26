13603dbc54e9046258fb30b4ef97250bb3f2ac62
jatin504 <jt872678@gmail.com>
2024-08-26 22:25:17 +0530

    add hover play button

--- src/components/Albemitem.js
+++ src/components/Albemitem.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import { albumData } from '../assets/images/assets';
-
+import React, { Component } from "react";
+import { albumData } from "../assets/images/assets";
+import { FaPlay } from "react-icons/fa";
 export default class Albemitem extends Component {
   render() {
     return (
-      <div className='flex overflow-auto'>
+      <div className="flex overflow-auto">
         {albumData.map((album) => (
           <div
             key={album.id}
-            className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26] m-2'
-            // style={{ backgroundColor: album.bgColor }}
+            className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26] m-2 relative group"
           >
-            <img src={album.image} alt={album.name} className='rounded-full object-cover  ' />
-            <p className='font-bold mt-2 mb-1'>{album.name}</p>
-            <p className='text-slate-200 text-sm'>{album.desc}</p>
+            <img
+              src={album.image}
+              alt={album.name}
+              className="rounded-full object-cover"
+            />
+            <p className="font-bold mt-2 mb-1">{album.name}</p>
+            <p className="text-slate-200 text-sm">{album.desc}</p>
+            <div className="bg-green-600 p-3 absolute bottom-16 right-1 flex items-center justify-center rounded-full w-14 text-lg opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0  transition-all duration-300">
+              <FaPlay className="text-[30px]" />
+            </div>
           </div>
         ))}
       </div>
