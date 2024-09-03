1c58dffb3ce61f9477c7b19ca432db36357cefdb
jatin504 <jt872678@gmail.com>
2024-09-03 21:15:56 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default class Navbar extends Component {
             </div>
             <div className='flex items-center gap-4'>
               <p className='pt-1 px-3 rounded-2xl text-[15px]'>Signup</p>
-              <p className='bg-white text-black text-[15px] px-4 py-1 cursor-pointer rounded-2xl hidden md:block'>Login</p>
+              <p className='bg-white text-black text-[15px] px-4 py-1 cursor-pointer rounded-2xl'>Login</p>
               {/* <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center'></p> */}
             </div>
         </div>
